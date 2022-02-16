ef0981b1cdc25090c5a1ceed390ad96ee94d6d27
QueueLi <62590518+QueueLi@users.noreply.github.com>
2022-02-16 15:14:06 +0300

    Update IntR.lua

--- IntR.lua
+++ IntR.lua
@@ -8,7 +8,7 @@ local event = require("event")
 local consoleLines = {}
 local eut = 0
 local timer = 60
-local Plaz = 98000 -- Начальный порог лазурита
+local Plaz = 10000 -- Начальный порог лазурита
 local sts = true
 local f = true
 local widgets = {
@@ -40,13 +40,13 @@ x = 6
 		local n = 1
 		for i = 1, 6 do
 		 
-		buffer.drawRectangle(x, y, 4, 2, 0xffc6c6, 0, " ") -- вывести	кнопочки
+		
 			for j = 1, 9 do			
 			widgets[n][1] = n			
 			widgets[n][3] = x
 			widgets[n][4] = y
 			n = n+1
-			buffer.drawRectangle(x, y, 4, 2, 0xffc6c6, 0, " ") -- вывести кнопочки
+			
 			x = x + 6
 			end
 		x = 6
@@ -72,7 +72,47 @@ buffer.drawText(16, 1, 0, "Контроль ядерного комплекса 
 		buffer.drawRectangle(4, 3, 56, 19, 0xBFBFBF, 0, " ") -- Фон реактора
 		
 		
+		x = 6 
+		y = 4
+		local i = 1
+		local j = 1
+		local n = 1
+		for i = 1, 6 do
+		 
+		buffer.drawRectangle(x, y, 4, 2, 0xffc6c6, 0, " ") -- вывести	кнопочки
+			for j = 1, 9 do			
+			widgets[n][1] = n			
+			widgets[n][3] = x
+			widgets[n][4] = y
+			n = n+1
+			buffer.drawRectangle(x, y, 4, 2, 0xffc6c6, 0, " ") -- вывести кнопочки
+			x = x + 6
+			end
+		x = 6
 		
+		y = y + 3
+		end
+		
+		x=54
+		
+		buffer.drawRectangle(x-6, y-3, 4, 2, 0xBFBFBF, 0, " ") -- Убрать крайние с крайние 53 
+buffer.drawRectangle(x, y-3, 4, 2, 0xBFBFBF, 0, " ") -- Убрать крайние с крайние 54	
+buffer.drawRectangle(6, y-3, 4, 2, 0xBFBFBF, 0, " ") -- Убрать крайние с крайние 47
+buffer.drawRectangle(12, y-3, 4, 2, 0xBFBFBF, 0, " ") -- Убрать крайние с крайние 46
+widgets[47][1] = widgets[52][1] -- поменять местами 48 с 52
+		widgets[47][3] = widgets[52][3]
+		widgets[47][4] = widgets[52][4]
+		
+		widgets[46][1] = widgets[51][1] -- поменять местами 47 с 51	
+		widgets[46][3] = widgets[51][3]
+		widgets[46][4] = widgets[51][4]
+		widgets[52][1] = widgets[47][1] -- поменять местами 48 с 52
+		widgets[52][3] = widgets[47][3]
+		widgets[52][4] = widgets[47][4]
+		
+		widgets[51][1] = widgets[46][1] -- поменять местами 47 с 51	
+		widgets[51][3] = widgets[46][3]
+		widgets[51][4] = widgets[46][4]
 
 		
 		
@@ -92,9 +132,13 @@ buffer.drawText(16, 1, 0, "Контроль ядерного комплекса 
 		
 		
 		
+		
 	 buffer.drawRectangle(61, 16, 27, 6, 0xFFFFFF, 0, " ")
 
-		buffer.drawRectangle(60, 23, 29, 4, 0xCCCCCC, 0, " ") -- Порог лазурита
+		buffer.drawRectangle(60, 23, 28, 4, 0xCCCCCC, 0, " ") -- Порог лазурита
+		
+		
+		
 
 		buffer.drawChanges()
 	end
@@ -111,7 +155,8 @@ buffer.drawText(16, 1, 0, "Контроль ядерного комплекса 
 		buffer.drawText(60, 24, 0x00FF00, "  |  ")
 		buffer.drawRectangle(60, 25, 4, 2, 0xCCCCCC, 0, " ") -- -
 		buffer.drawText(60, 25, 0xFF0000, " ___ ")
-    
+		buffer.drawRectangle(60, 27, 28, 2, 0xCCCCCC, 0, " ") -- Таймер
+		
     buffer.drawChanges()
 end
 
@@ -174,6 +219,8 @@ local function wait(n)
 	if  Click() == nil then
 		t = t + 0.1
 		timer = timer + 0.1
+		buffer.drawText(60, 28, 0, 'Таймер : ' .. round(60 - timer))
+		buffer.drawChanges()
 		end
 	end
 end
@@ -202,11 +249,16 @@ local function stop()
 					
 	end
 	
-					local function checkRe()
+	local function checkRe()
 					wait(0.1)
+					while not com.isAvailable("reactor_chamber")  do
+						  message("Ошибика! Нет реакторов!")
+						  computer.beep(500, 1)
+						  wait(3)
+						  end
 					eut = 0
 					
-					
+					knopoch()
 					
 					z = 0
 				for address, componentType in com.list("react")  do
@@ -216,17 +268,24 @@ local function stop()
 				
 							widgets[z][2] = com.invoke(widgets[z][5], "getReactorEUOutput")
 							eut = eut + widgets[z][2]
+					
+				
+				
+				if widgets[z][2] == 0 and widgets[z][6] then
+				buffer.drawRectangle(widgets[z][3], widgets[z][4], 4, 2, 0xFFFF00, 0, " ") 
+				else
 				buffer.drawRectangle(widgets[z][3], widgets[z][4], 4, 2, 0x00FF00, 0, " ")
+				
 				end
-					while not com.isAvailable("reactor_chamber")  do
-						  message("Ошибика! Нет реакторов!")
-						  computer.beep(500, 1)
-						  wait(3)
-						  end		  
+				
+				end
+				
+				
 					i=1
 					buffer.drawRectangle(61, 19, 27, 3, 0x111111, 0, " ") --Колличество еу в тик
 					buffer.drawText(66, 20, 0x00FF00, 'EU/t : '.. round(eut))
 					buffer.drawChanges()
+					wait(0.1)
 					return 1
 					end
 	
@@ -278,20 +337,7 @@ message("Настройка компонентов")
 wait(0.1)
 		
 		
-		widgets[47][1] = widgets[52][1] -- поменять местами 48 с 52
-		widgets[47][3] = widgets[52][3]
-		widgets[47][4] = widgets[52][4]
 		
-		widgets[46][1] = widgets[51][1] -- поменять местами 47 с 51	
-		widgets[46][3] = widgets[51][3]
-		widgets[46][4] = widgets[51][4]
-		widgets[52][1] = widgets[47][1] -- поменять местами 48 с 52
-		widgets[52][3] = widgets[47][3]
-		widgets[52][4] = widgets[47][4]
-		
-		widgets[51][1] = widgets[46][1] -- поменять местами 47 с 51	
-		widgets[51][3] = widgets[46][3]
-		widgets[51][4] = widgets[46][4]
 buffer.drawChanges()
 
 z = 0
@@ -320,7 +366,7 @@ end
 message("Успешно, запускаю реакторы")
 start()
 	while (checkRe() == 1 or  checkMe() == 1 or  checkLaz() == 1) and sts == true  do
-	message("Сек до чек " .. 60 - timer)
+	--message("Сек до чек " .. 60 - timer)
 	buffer.drawRectangle(61, 19, 27, 3, 0x111111, 0, " ") --Колличество еу в тик
 	buffer.drawText(66, 20, 0x00FF00, 'EU/t : '.. round(eut))
 	buffer.drawChanges()
