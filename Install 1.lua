f2ee20d2eb8450f45ec71feb9817cb639fda3ab9
QueueLi <62590518+QueueLi@users.noreply.github.com>
2022-02-17 11:48:18 +0300

    Update Install 1.lua

--- Install 1.lua
+++ Install 1.lua
@@ -28,9 +28,12 @@ local libs = {
 --wget -f https://raw.githubusercontent.com/QueueLi/Reactor-1.12.2/main/r.txt /home/Reactor.lua
 shell.execute("md /home/images/reactor/")
 shell.execute("md md /lib/FormatModules")
+shell.execute("rm .shrc")
 shell.execute("wget -f https://raw.githubusercontent.com/QueueLi/Reactor-1.12.2/main/r.txt" .. " /home/Reactor.lua")
 shell.execute("wget -f https://raw.githubusercontent.com/QueueLi/Reactor-1.12.2/main/IntR.lua" .. " /home/IntR.lua")
-
+shell.execute("wget -f https://raw.githubusercontent.com/QueueLi/Reactor-1.12.2/main/reboot" .. " /home/reboot")
+shell.execute("wget -f https://raw.githubusercontent.com/QueueLi/Reactor-1.12.2/main/off" .. " /home/off")
+shell.execute("wget -f https://raw.githubusercontent.com/QueueLi/Reactor-1.12.2/main/.shrc" .. " /home/.shrc")
 
 for i = 1, #libs do
 		shell.execute("wget -f " ..  libs[i].url .. " " .. libs[i].path)
